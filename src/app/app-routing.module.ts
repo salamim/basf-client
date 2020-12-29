131b572f6dec5fbdd0d4e56d501b13db2cf707ca
Mutalib Salami <salamim@imalastech.com>
2020-12-28 20:37:34 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddressFormComponent } from './components/address-form/address-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'address', component: AddressFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
